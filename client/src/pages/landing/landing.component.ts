5b7edfb2afd581b4a6bdd1ac7a5ff37768b709bd
Rakesh7502 <93503869+Rakesh7502@users.noreply.github.com>
2024-10-29 21:21:23 +0530

    Update landing.component.ts

--- client/src/pages/landing/landing.component.ts
+++ client/src/pages/landing/landing.component.ts
@@ -3,6 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { FeatureCardComponent } from '../../components/feature-card/feature-card.component';
 
+//import { FeatureCardComponent } from '../../components/feature-card/feature-card.component';
+
 @Component({
   selector: 'app-landing',
   standalone: true,
