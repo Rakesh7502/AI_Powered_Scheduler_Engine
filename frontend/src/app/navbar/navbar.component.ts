ebf299891ff7aa9673d98d696e999400232eb12c
Rakesh7502 <93503869+Rakesh7502@users.noreply.github.com>
2024-11-05 23:01:20 +0530

    Updated navBar

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -3,7 +3,10 @@
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule, NgIf } from '@angular/common';
 
-
+//   import { Component } from '@angular/core';
+//   import {RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+// import { MatButtonModule } from '@angular/material/button';
+// import { CommonModule, NgIf } from '@angular/common';
 
   @Component({
     selector: 'app-navbar',
