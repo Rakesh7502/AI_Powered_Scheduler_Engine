1e97ce6145c2482cad5caa7501396587ab3a1c2e
Rakesh7502 <93503869+Rakesh7502@users.noreply.github.com>
2024-11-10 22:33:07 +0530

    Update workspaces.py

--- server/app/services/workspaces.py
+++ server/app/services/workspaces.py
@@ -20,3 +20,19 @@ def get_workspace_content(data: GetWorkspaces):
         )
 
     return resp.json()
+ ##   def get_workspace_content(data: GetWorkspaces):
+#    resp = requests.get(
+  #      f"{development.DBS_WORKSPACE_URL}/api/2.0/workspace/list",
+    #    json=data.model_dump(),
+     #   headers=development.headers,
+ #   )
+
+#    if resp.status_code != 200:
+    #    raise CustomException(
+   #         error_code=resp.json()["error_code"],
+      #      details=resp.json()["message"],
+       #     message="Failed to retrieve the content of the given path.",
+        )
+
+ #   return resp.json()
+    
