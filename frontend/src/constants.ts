65417009cc8643741ffd5aca44ec2a9c15db674d
Rakesh7502 <93503869+Rakesh7502@users.noreply.github.com>
2024-11-13 23:16:06 +0530

    Update constants.ts

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -2,3 +2,4 @@ export const backendUrl = 'http://localhost:8080/api/v1';
 // export const backendUrl = 'http://localhost:8080/api/v1';
 // export const backendUrl = 'http://localhost:8080/api/v1';
 // export const backendUrl = 'http://localhost:8080/api/v1';
+//export const backendUrl = 'http://localhost:8080/api/v1
